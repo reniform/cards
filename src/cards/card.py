459b7feea841024f6077830b7d192850151d571c
Harper Phelan <netcasper5005@gmail.com>
2025-11-13 22:44:30 -0600

    initial commit

---
+++ src/cards/card.py
@@ -0,0 +1,19 @@
+class MonCardClass:
+    monTitle:      str
+    monStage:      int
+    monLevel:      int
+
+    monHealth:     int
+    monRetreatVal: int
+    
+    monType:       int
+    monWeakType:   int
+    monResistType: int
+
+    monDexNum:     int
+    monDexHeight:  float
+    monDexWeight:  float
+    monDexEntry:   str
+
+    def __init__(self):
+        pass``
