df38826631dd5f0ee0d28449ae3b481ebd7598bd
Harper Phelan <netcasper5005@gmail.com>
2025-11-15 01:51:51 -0600

    Expand enums: rarity and stages

--- src/cards/enums.py
+++ src/cards/enums.py
@@ -12,9 +12,15 @@ class StageType(Enum):
     """
     'StageType' lists the stages in incremental order.
     """
-    BASIC    = 2
-    STAGEONE = 3
-    STAGETWO = 4
+    BASIC       =  2
+    STAGEONE    =  3
+    STAGETWO    =  4
+    EXRS        =  5
+    EXBW        =  6
+    EXSV        =  7
+    MEGAEXXY    =  8
+    MEGAEXSV    =  9
+    TERA        = 10
 
 class ManaType(Enum):
     """
@@ -31,3 +37,18 @@ class ManaType(Enum):
     DRAGON      = 'dragon'
     FAIRY       = 'fairy'
     COLORLESS   = 'colorless'
+
+class RarityType(Enum):
+    COMMON          = 'common'
+    UNCOMMON        = 'uncommon'
+    RARE            = 'rare'
+    HOLO_RARE       = 'holo_rare'
+    DOUBLE_RARE     = 'double_rare'
+    ULTRA_RARE      = 'ultra_rare'
+    SECRET_RARE     = 'secret_rare'
+    ACE_SPEC_RARE   = 'ace_spec'
+    ILLUSTRATION    = 'illustration'
+    SPECIAL_ILL     = 'special_ill'
+    HYPER_RARE      = 'hyper_rare'
+    MEGA_HYPER_RARE = 'mega_hyper_rare'
+    PROMO           = 'promo'
